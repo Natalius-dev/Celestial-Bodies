de2e2e8b93a39fdeddcdbcf35d7935a4a4ff6a56
Natalius <nataliusgabriel@gmail.com>
2023-03-21 22:33:07 +0700

    revised navbar to be easier to click

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -33,10 +33,10 @@
     });
 </script>
 
-<div id="navbar" style="font-family: 'Martian Mono', monospace;" class="flex flex-row font-thin [&>div]:hidden [&>div]:sm:flex [&>div]:justify-center [&>div]:duration-300 [&>div]:flex-grow [&>div]:px-5 [&>div]:py-4 [&>div]:text-white hover:[&>div]:text-gray-900 [&>div]:bg-slate-800 hover:[&>div]:bg-slate-500 hover:[&>div>span>a]:underline hover:[&>div>span>a]:text-black hover:[&>div>span>a]:duration-300">
-    <div id="expand-btn" class="!flex !justify-start sm:!hidden"><span><i class="fa-solid fa-bars"></i></span></div>
-    <div name="expand"><span><a href="#">Home</a></span></div>
-    <div name="expand"><span><a href="#">About</a></span></div>
-    <div name="expand"><span><a href="#">FAQ</a></span></div>
-    <div name="expand"><span><a href="#">Contact</a></span></div>
+<div id="navbar" style="font-family: 'Martian Mono', monospace;" class="flex flex-row font-thin [&>div]:hidden [&>div]:sm:flex [&>div]:justify-center [&>div]:duration-300 [&>div]:flex-grow [&>div]:text-white hover:[&>div]:text-gray-900 [&>div]:bg-slate-800 hover:[&>div]:bg-slate-500 hover:[&>div>a>span]:underline hover:[&>div>a>span]:text-black hover:[&>div>a>span]:duration-300 [&>div>a]:w-full [&>div>a]:h-full [&>div>a]:mx-5 [&>div>a]:my-4 [&>div>a]:text-center">
+    <div id="expand-btn" class="!flex !justify-start sm:!hidden"><span class="mx-5 my-4"><i class="fa-solid fa-bars"></i></span></div>
+    <div name="expand"><a href="#"><span>Home</span></a></div>
+    <div name="expand"><a href="#"><span>About</span></a></div>
+    <div name="expand"><a href="#"><span>FAQ</span></a></div>
+    <div name="expand"><a href="#"><span>Contact</span></a></div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
-<div style="font-family: 'Poppins', sans-serif;" class="w-full h-full flex p-5 flex-col">
-    <div class="flex-col [&>*]:text-white">
+<div style="font-family: 'Poppins', sans-serif;" class="w-full h-full flex p-10 flex-col">
+    <div class="flex-col [&>*]:text-white mb-2">
         <h1 class="font-light text-5xl pb-1">Star Systems</h1>
         <h2 class="font-extralight text-2xl pt-1">by Natalius</h2>
     </div>
-    
+    <hr class="my-3">
+    <div class="flex-col [&>*]:text-white my-2">
+        <p class="font-light text-lg">Get started by importing an existing star systems or explore one of our default systems.</p>
+    </div>
+    <div class="flex-col [&>*]:text-white my-2">
+        <input class="flex" type="file">
+        <input class="flex" type="submit">
+    </div>
 </div>
